2458c4f7c0d724210e408b02cfdbabf661a31d8a
Sebastien Bariteau <numkem@numkem.org>
2023-11-16 16:18:19 -0500

    rewrite to add system to many items

--- flake.nix
+++ flake.nix
@@ -3,45 +3,58 @@
 
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
 
-  outputs = { self, nixpkgs }: rec {
-    packages.x86_64-linux = let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      traefik-keymate = pkgs.buildGoModule {
-        pname = "traefik-keymate";
-        version = "0.1.0";
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
 
-        src = ./.;
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in rec {
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in rec {
+          traefik-keymate = pkgs.buildGoModule {
+            pname = "traefik-keymate";
+            version = "0.1.0";
 
-        submodules = [ "server" ];
+            src = ./.;
 
-        vendorHash = "sha256-9ksyoevxfieIZJE8EVdgTloEHf5HX0A2MqF+ZAvUc1U=";
+            submodules = [ "server" ];
 
-        postInstall = ''
-          mv $out/bin/server $out/bin/traefik-keymate
-        '';
-      };
-    };
+            vendorHash = "sha256-9ksyoevxfieIZJE8EVdgTloEHf5HX0A2MqF+ZAvUc1U=";
 
-    devShells.x86_64-linux.default = let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in pkgs.mkShell { buildInputs = with pkgs; [ etcd go gopls ]; };
+            postInstall = ''
+              mv $out/bin/server $out/bin/traefik-keymate
+            '';
+          };
 
-    overlays.default =
-      (final: prev: { inherit (packages.x86_64-linux) traefik-keymate; });
+          default = traefik-keymate;
+        });
 
-    nixosConfigurations.test = let system = "x86_64-linux";
-    in nixpkgs.lib.nixosSystem rec {
-      inherit system;
-      pkgs = import nixpkgs {
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default =
+            pkgs.mkShell { buildInputs = with pkgs; [ etcd go gopls ]; };
+        });
+
+      overlays = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default =
+            (final: prev: { inherit (packages.${system}) traefik-keymate; });
+        });
+
+      nixosConfigurations.test = let system = "x86_64-linux";
+      in nixpkgs.lib.nixosSystem rec {
         inherit system;
-        overlays = [ overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlays.${system}.default ];
+        };
+        modules = [ nixosModules.default ./nix/configuration-test.nix ];
       };
-      modules = [ nixosModules.default ./nix/configuration-test.nix ];
-    };
 
-    nixosModules.default = import ./nix/modules/default.nix;
-  };
+      nixosModules.default = import ./nix/modules/default.nix;
+    };
 }
