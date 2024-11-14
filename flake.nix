73f7b436428f2c461e73c9624ed6a3a8be8c2425
Sebastien Bariteau <numkem@numkem.org>
2024-11-14 17:15:28 -0500

    bump version 0.4.0

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
         rec {
           traffikey = pkgs.buildGoModule {
             pname = "traffikey";
-            version = "0.3.0";
+            version = "0.4.0";
 
             src = ./.;
 
